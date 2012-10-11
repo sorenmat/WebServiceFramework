42ff18641298c2e24a78bf5e1c7c2718122aea55
Soren Mathiasen <sorenm@mymessages.dk>
2012-10-11 16:06:00 +0200

    made test work

--- src/test/com/scalaprog/ws/test/ListTestServiceTest.scala
+++ src/test/com/scalaprog/ws/test/ListTestServiceTest.scala
@@ -2,18 +2,19 @@ package com.scalaprog.ws.test
 
 import org.scalatest.FunSuite
 import dispatch._
-import com.scalaprog.{WebServiceServer, MyHelloWorld}
+import com.scalaprog.{ListTestService, WebServiceServer, MyHelloWorld}
 
 
-class HelloWorldServiceTest extends FunSuite {
+class ListTestServiceTest extends FunSuite {
 
   test("In Hello world webservice wsdl should contain MyHelloWorldServiceSoapBinding") {
     val server = new WebServiceServer("127.0.0.1", "http://localhost", "9090")
-    server.register("Hello", new MyHelloWorld)
+    server.register("ListService", new ListTestService)
     server.start()
 
-    val svc = url("http://localhost:9090/Hello")
+    val svc = url("http://localhost:9090/ListService")
     val wsdl = Http(svc OK as.String)().toString
+    println(wsdl)
     assert(wsdl contains ("MyHelloWorldServiceSoapBinding"))
     server.stop()
   }
